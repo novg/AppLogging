6247d16ff6770dc89c3541cda2a93dbda2ccf5a2
novg <novg.novg@gmail.com>
2020-05-01 14:51:48 +0700

    Add project files.

---
+++ Program.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using NLog;
+
+namespace AppLogging
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Logger logger = LogManager.GetCurrentClassLogger();
+
+            logger.Trace("trace message");
+            logger.Debug("debug message");
+            logger.Info("info message");
+            logger.Warn("warn message");
+            logger.Error("error message");
+            logger.Fatal("fatal message");
+
+            logger.Error(new DivideByZeroException(), "value was not calculate");
+        }
+    }
+}
